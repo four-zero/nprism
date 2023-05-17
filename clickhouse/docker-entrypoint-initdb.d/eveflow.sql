d91e372698ba2b5f4aa1bb31034262d831154d71
4ling.cn <4ling.cn@gmail.com>
2023-05-17 23:44:22 +0800

    clickhouse:     1. 表结构重构，再使用JSON对象，而是使用Tuple详细描述每个字段     2. 增加view_service物化视图，用于展示IP资产的活跃服务数据

--- clickhouse/docker-entrypoint-initdb.d/eveflow.sql
+++ clickhouse/docker-entrypoint-initdb.d/eveflow.sql
@@ -1,43 +1,82 @@
-SET allow_experimental_object_type = 1;
-
 CREATE DATABASE IF NOT EXISTS suricata;
 
 CREATE TABLE suricata.eveflow
 (
     `timestamp` DateTime64(9, 'Asia/Shanghai'),
     `flow_id` UInt64,
-    `parent_id` Nullable(UInt64),
-    `in_iface` Nullable(String),
+    `parent_id` UInt64,
+    `in_iface` LowCardinality(String),
     `src_ip` String,
     `dest_ip` String,
-    `src_port` Nullable(UInt64),
-    `dest_port` Nullable(UInt64),
-    `proto` String,
-    `event_type` String,
-    `app_proto` Nullable(String),
-    `icmp_type` Nullable(UInt64),
-    `icmp_code` Nullable(UInt64),
-    `response_icmp_type` Nullable(UInt64),
-    `response_icmp_code` Nullable(UInt64),
-    `spi` Nullable(UInt64),
-    `app_proto_ts` Nullable(String),
-    `app_proto_tc` Nullable(String),
-    `app_proto_orig` Nullable(String),
-    `app_proto_expected` Nullable(String),
-    `community_id` Nullable(String),
-    `tenant_id` Nullable(String),
-    `flow` JSON,
-    `ether` JSON,
-    `tcp` JSON,
+    `src_port` UInt16,
+    `dest_port` UInt16,
+    `proto` LowCardinality(String),
+    `event_type` LowCardinality(String),
+    `app_proto` LowCardinality(String),
+    `icmp_type` UInt8,
+    `icmp_code` UInt8,
+    `response_icmp_type` UInt8,
+    `response_icmp_code` UInt8,
+    `spi` UInt32,
+    `app_proto_ts` LowCardinality(String),
+    `app_proto_tc` LowCardinality(String),
+    `app_proto_orig` LowCardinality(String),
+    `app_proto_expected` LowCardinality(String),
+    `community_id` String,
+    `tenant_id` LowCardinality(String),
+    `ether` Tuple(
+        dest_macs Array(String),
+        src_macs Array(String)
+    ),
+    `flow` Tuple(
+        pkts_toserver UInt64,
+        pkts_toclient UInt64,
+        bytes_toserver UInt64,
+        bytes_toclient UInt64,
+        bypassed Tuple(
+            bytes_toclient UInt64,
+            bytes_toserver UInt64,
+            pkts_toclient UInt64,
+            pkts_toserver UInt64
+        ),
+        start String,
+        end String,
+        age UInt64,
+        emergency Bool,
+        bypass LowCardinality(String),
+        state LowCardinality(String),
+        reason LowCardinality(String),
+        alerted Bool,
+        wrong_thread Bool,
+        action LowCardinality(String)
+    ), 
+    `tcp` Tuple(
+        tcp_flags String,
+        tcp_flags_ts String,
+        tcp_flags_tc String,
+        syn Bool,
+        fin Bool,
+        rst Bool,
+        psh Bool,
+        ack Bool,
+        urg Bool,
+        enc Bool,
+        cwr Bool,
+        state LowCardinality(String),
+        ts_gap Bool,
+        tc_gap Bool,
+        ts_max_regions UInt16,
+        tc_max_regions UInt16
+    ),
 
-    INDEX flow_id_idx flow_id TYPE bloom_filter GRANULARITY 8,
+    INDEX tenant_id_idx app_proto TYPE set(1024) GRANULARITY 8,
     INDEX in_facle_idx in_iface TYPE set(1024) GRANULARITY 8,
+    INDEX flow_id_idx flow_id TYPE bloom_filter GRANULARITY 8,
     INDEX src_ip_idx src_ip TYPE bloom_filter GRANULARITY 8,
     INDEX dest_ip_idx dest_ip TYPE bloom_filter GRANULARITY 8,
     INDEX src_port_idx src_port TYPE bloom_filter GRANULARITY 8,
     INDEX dest_port_idx dest_port TYPE bloom_filter GRANULARITY 8,
     INDEX proto_idx proto TYPE set(1024) GRANULARITY 8,
-    INDEX event_type_idx event_type TYPE set(1024) GRANULARITY 8,
     INDEX app_proto_idx app_proto TYPE set(1024) GRANULARITY 8
 )
 ENGINE = MergeTree()
@@ -51,12 +90,11 @@ CREATE VIEW suricata.view_service
 AS
 SELECT
     dest_ip,
-    groupUniqArray(10)(proto) AS service_proto,
-    groupUniqArray(10)(dest_port) AS service_port,
-    min(flow.start) AS min_timestamp,
-    max(flow.start) AS max_timestamp
+    groupUniqArray(8)(proto) AS service_proto,
+    groupUniqArray(16)(dest_port) AS service_port,
+    min(flow.start) AS service_active_earliest,
+    max(flow.start) AS service_active_latest
 FROM suricata.eveflow
 WHERE (flow.pkts_toclient > 0) AND (flow.state != 'new')
 GROUP BY dest_ip
-ORDER BY max_timestamp DESC;
-
+ORDER BY service_active_latest DESC;
--- clickhouse/docker-entrypoint-initdb.d/init.sh
+++ clickhouse/docker-entrypoint-initdb.d/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-clickhouse-client --port 9003 -u clickhouse --password 00000000 --multiquery </docker-entrypoint-initdb.d/drop.sql
+clickhouse-client -u clickhouse --password 00000000 --multiquery </docker-entrypoint-initdb.d/drop.sql
 
-clickhouse-client --port 9003 -u clickhouse --password 00000000 --multiquery </docker-entrypoint-initdb.d/eveflow.sql
+clickhouse-client -u clickhouse --password 00000000 --multiquery </docker-entrypoint-initdb.d/eveflow.sql
 
