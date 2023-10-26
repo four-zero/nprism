c4c6b11b1811d1a5d13748a40348ff40029e73b9
four-zero <4ling.cn@gmail.com>
2023-10-26 15:55:00 +0800

    script: 更新 image save/load 脚本

--- script/docker_load.sh
+++ script/docker_load.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker image load -i images/jasonish.suricata.6.0.tar
+docker image load -i images/jasonish.suricata.7.0.tar
 docker image load -i images/bitnami.redis.7.0.tar
 docker image load -i images/timberio.vector.0.29.X-debian.tar
 docker image load -i images/bitnami.clickhouse.23.tar
--- script/docker_save.sh
+++ script/docker_save.sh
@@ -3,5 +3,5 @@
 docker image save bitnami/redis:7.0 -o images/bitnami.redis.7.0.tar
 docker image save bitnami/clickhouse:23 -o images/bitnami.clickhouse.23.tar
 docker image save timberio/vector:0.29.X-debian -o images/timberio.vector.0.29.X-debian.tar
-docker image save jasonish/suricata:6.0 -o images/jasonish.suricata.6.0.tar
+docker image save jasonish/suricata:7.0 -o images/jasonish.suricata.7.0.tar
 #docker image save bitnami/grafana:9 -o images/bitnami.grafana.9.tar
